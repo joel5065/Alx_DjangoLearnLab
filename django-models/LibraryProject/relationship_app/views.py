306179a1d6d87e4a5bf82920e6918266df0a7375
Joel Kouakou <kouakoujoel7@gmail.com>
2025-02-20 16:23:59 +0000

    editing views file

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .models import Book, Author, Librarian, Library
+from .models import Book
+from .models import Library
 from django.views.generic import DetailView
 
 # Create your views here.
