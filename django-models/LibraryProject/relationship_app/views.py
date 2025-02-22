36fe0fdcdb31341300c5c924767ee7f2d68954b9
Joel Kouakou <kouakoujoel7@gmail.com>
2025-02-22 12:33:07 +0000

    adding usercreationform

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
 from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
