cd6f3768aa3f5bce1d29d124dae1edd609a7ec3c
Joel Kouakou <kouakoujoel7@gmail.com>
2025-02-22 23:25:07 +0000

    modifying the views file

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.decorators import user_passes_test, permission_required
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import permission_required
 from .models import Book
 from .models import Library
 from .forms import BookForm
