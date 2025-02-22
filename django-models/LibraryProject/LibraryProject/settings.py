ae0aaf201630469eafb8e5f955d9eca580753090
Joel Kouakou <kouakoujoel7@gmail.com>
2025-02-22 13:26:58 +0000

    modifying settings

--- django-models/LibraryProject/LibraryProject/settings.py
+++ django-models/LibraryProject/LibraryProject/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'LibraryProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS':  [BASE_DIR / 'LibraryProject/templates'],
+        'DIRS':  [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,7 +119,7 @@ USE_TZ = True
 
 STATIC_URL =  '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'LibraryProject/static',  # Add your static files directory here
+    BASE_DIR / 'static',  # Add your static files directory here
 ]
 
 # Default primary key field type
