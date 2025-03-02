c60b9254177defbd2d52754c45acce3d623c9c46
Joel Kouakou <kouakoujoel7@gmail.com>
2025-03-02 13:40:07 +0000

    creating view decorator

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
-
+from .models import Book
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import permission_required
 # Create your views here.
+def list_books(request):
+    books = Book.objects.all()
+    context = {'list_books': books}
+    return render(request, 'bookshelf/list_books.html', context)
+
+@permission_required('bookshelf.can_delete_book')
+def delete_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('list_books')
+    return render(request, 'bookshelf/book_confirm_delete.html', {'book': book})
