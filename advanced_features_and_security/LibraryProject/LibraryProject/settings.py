b307b97ac1867015a0613c098773cf928826b4b5
Joel Kouakou <kouakoujoel7@gmail.com>
2025-03-02 01:40:33 +0000

    setting up things

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Configure Django to user custom user
+# Configure Django to use custom user
 
 AUTH_USER_MODEL = 'bookshelf.CustomUser'
 
