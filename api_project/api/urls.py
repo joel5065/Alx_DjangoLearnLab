7a7a9974bc1dcf681151ec281e8dd6cff141e6b7
Joel Kouakou <kouakoujoel7@gmail.com>
2025-03-08 10:43:58 +0000

    setting the CRUD

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,8 +1,15 @@
-from django.urls import path
-from .views import BookList
+from django.urls import path, include
+from .views import BookList, BookViewSet
+from rest_framework import DefaultRouter
+
+
+router = DefaultRouter()
+router.register(r'Book',BookViewSet)
 
 urlpatterns = [
 
-    path('book/', BookList.as_view(), name='book-list')
+    path('book/', BookList.as_view(), name='book-list'),
+
+    path('', include(router.urls)),  
 
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
 
@@ -6,3 +6,8 @@ from .serializers import BookSerializer
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer = BookSerializer
