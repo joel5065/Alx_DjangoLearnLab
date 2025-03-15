53b89dafbbe909339bd8c049d33c3125cc451cf9
Joel Kouakou <kouakoujoel7@gmail.com>
2025-03-15 11:12:03 +0000

    creating view

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions, filters
+from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['author', 'title']
+
+class BookDetailView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ['title', 'author']
+
+class BookCreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    #Customization example: Overriding create method for additional logic or validation
+    def perform_create(self, serializer):
+        # Additional logic before saving, e.g., logging, validation
+        serializer.save()
+
+class BookUpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    #Customization example: Overriding update method
+    def perform_update(self, serializer):
+        #Additional logic before saving, e.g., logging, validation
+        serializer.save()
+
+class BookDeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
