d4f586844079403cd4cfed3b5161c8bdef8268c0
Joel Kouakou <kouakoujoel7@gmail.com>
2025-03-19 11:33:40 +0000

    creation of comment model

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -15,3 +15,13 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('post-detail', kwargs={'pk': self.pk})
+    
+class Comment(models.Model):
+    content = models.TextField(max_length=200)
+    created_at = models.DateField(auto_now=True)
+    updated_at = models.DateField(auto_now=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.content
