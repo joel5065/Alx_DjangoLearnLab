80e06ec6c02fe289e350c2060983a213ed8f6892
Joel Kouakou <kouakoujoel7@gmail.com>
2025-03-19 12:12:11 +0000

    create comment model and views

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
-from .views import PostCreateView, PostDeleteView, PostDetailView, PostListView,PostUpdateView
+from .views import PostCreateView, PostDeleteView, PostDetailView, PostListView,PostUpdateView, CommentUpdateView, CommentDeleteView
 
 urlpatterns = [
     path('register/', views.register, name='register'),
-    path('', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
     path('profile/', views.profile, name='profile'),
     path('posts/', PostListView.as_view(), name='post-list'),
@@ -13,6 +13,7 @@ urlpatterns = [
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('post/<int:post_id>/comments/new/', views.add_comment, name='add-comment'),
     path('comment/<int:pk>/edit/', CommentUpdateView.as_view(), name='comment-update'),
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
 ]
