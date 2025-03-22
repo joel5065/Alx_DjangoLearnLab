6ce2f44f5b089fd84eeb004e3148455c07087f76
Joel Kouakou <kouakoujoel7@gmail.com>
2025-03-22 15:53:58 +0000

    creation of widgets

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Post, Comment, Tag
+from taggit.forms import TagWidget, widgets
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -11,30 +12,12 @@ class CustomUserCreationForm(UserCreationForm):
         fields = ("username", "email", "password", "password2")
 
 class PostForm(forms.ModelForm):
-    tags = forms.CharField(required=False) # Form field for tags
-
     class Meta:
         model = Post
         fields = ['title', 'content', 'tags']
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        if self.instance and self.instance.pk:
-            self.fields['tags'].initial = ', '.join(tag.name for tag in self.instance.tags.all())
-
-    def save(self, commit=True):
-        post = super().save(commit=False)
-        if commit:
-            post.save()
-
-        tag_names = [tag.strip() for tag in self.cleaned_data['tags'].split(',') if tag.strip()]
-        tags = []
-        for tag_name in tag_names:
-            tag, created = Tag.objects.get_or_create(name=tag_name)
-            tags.append(tag)
-
-        post.tags.set(tags)
-        return post
+        widgets = {
+            'tags': TagWidget(),  # Use TagWidget for the tags field
+        }
 
 class CommentForm(forms.ModelForm):
     class Meta:
