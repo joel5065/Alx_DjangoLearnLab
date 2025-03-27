5c7c4532813cf4fcd573b605db70946b941e415f
Joel Kouakou <kouakoujoel7@gmail.com>
2025-03-27 13:38:32 +0000

    creation of post et comment models

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -12,3 +12,30 @@ class CustomUser(AbstractUser):
         return self.username
     
 
+class Post(models.Model):
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='posts')
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def _str_(self):
+        return f"{self.title} by {self.author.username}"
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def _str_(self):
+        return f"Comment by {self.author.username} on {self.post.title}"
+    
+
