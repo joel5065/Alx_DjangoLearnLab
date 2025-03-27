8a70b4b4c2cd555e1e8c2d20644a7ec3b583efb2
Joel Kouakou <kouakoujoel7@gmail.com>
2025-03-27 13:08:45 +0000

    adding urls in both files and create views to manage login and register

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate
+from .serializers import UserSerializer
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
 
 # Create your views here.
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def register(request):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        user = serializer.save()
+        return Response({
+            'user': UserSerializer(user).data,
+            'message': 'User registered successfully'
+        })
+    return Response("errors during user creation")
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def login(request):
+    username = request.data.get(['username'])
+    password = request.data.get(['password'])
+    user = authenticate(username=username, password=password)
+
+    if user:
+        token, _=Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user': UserSerializer(user).data
+        })
+    return Response("Error: Invalid credentials!")
