6a3e69a7c1622843889d599fc565e6bdf29c9e55
Joel Kouakou <kouakoujoel7@gmail.com>
2025-03-28 20:35:47 +0000

    changing follow and unfollow views using generics APIView

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import register_user, login, follow_user, unfollow_user, UserListView, UserDetailView, UserCreateView,UserDeleteView, UserUpdateView
+from .views import register_user, login, FollowUserView, UnfollowUserView, UserListView, UserDetailView, UserCreateView,UserDeleteView, UserUpdateView
 
 
 
@@ -12,7 +12,7 @@ urlpatterns = [
     path('users/create/', UserCreateView.as_view(), name='user-create'),
     path('users/update/<int:pk>', UserUpdateView.as_view(), name='user-update'),
     path('users/delete/<int:pk>', UserDeleteView.as_view(), name='user-delete'),
-    path('follow/<int:user_id>/', follow_user, name='follow_user'),
-    path('unfollow/<int:user_id>/', unfollow_user, name='unfollow_user'),
+    path('follow/<int:user_id>/', FollowUserView.as_view(), name='follow_user'),
+    path('unfollow/<int:user_id>/', UnfollowUserView.as_view(), name='unfollow_user'),
 
 ]
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -79,20 +79,22 @@ class UserDeleteView(generics.DestroyAPIView):
     permission_classes = [permissions.IsAuthenticated]
 
 
-@api_view(['POST'])
-@permission_classes([permissions.IsAuthenticated])
-def follow_user(request, user_id):
-    user_to_follow = get_object_or_404(CustomUser, pk=user_id)
-    if request.user == user_to_follow:
-        return Response({"error": "You cannot follow yourself."}, status=status.HTTP_400_BAD_REQUEST)
-    request.user.following.add(user_to_follow)
-    return Response({"message": f"You are now following {user_to_follow.username}."}, status=status.HTTP_200_OK)
+class FollowUserView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
 
-@api_view(['POST'])
-@permission_classes([permissions.IsAuthenticated])
-def unfollow_user(request, user_id):
-    user_to_unfollow = get_object_or_404(CustomUser, pk=user_id)
-    if request.user == user_to_unfollow:
-        return Response({"error": "You cannot unfollow yourself."}, status=status.HTTP_400_BAD_REQUEST)
-    request.user.following.remove(user_to_unfollow)
-    return Response({"message": f"You have unfollowed {user_to_unfollow.username}."}, status=status.HTTP_200_OK)
+    def post(self, request, user_id):
+        user_to_follow = get_object_or_404(CustomUser, pk=user_id)
+        if request.user == user_to_follow:
+            return Response({"error": "You cannot follow yourself."}, status=status.HTTP_400_BAD_REQUEST)
+        request.user.following.add(user_to_follow)
+        return Response({"message": f"You are now following {user_to_follow.username}."}, status=status.HTTP_200_OK)
+
+class UnfollowUserView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, user_id):
+        user_to_unfollow = get_object_or_404(CustomUser, pk=user_id)
+        if request.user == user_to_unfollow:
+            return Response({"error": "You cannot unfollow yourself."}, status=status.HTTP_400_BAD_REQUEST)
+        request.user.following.remove(user_to_unfollow)
+        return Response({"message": f"You have unfollowed {user_to_unfollow.username}."}, status=status.HTTP_200_OK)
