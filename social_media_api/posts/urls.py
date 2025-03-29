24c829ed72f1fc69727ac3d8d5481bc2fff21be8
Joel Kouakou <kouakoujoel7@gmail.com>
2025-03-29 12:07:25 +0000

    adding feed url in posts/urls

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import PostViewSet, CommentViewset
+from .views import PostViewSet, CommentViewset, FeedView
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
@@ -11,5 +11,6 @@ p_router.register(r'comments', CommentViewset, basename='post-comment')
 urlpatterns = [
     path('posts/', include(router.urls)),
     path('posts/<int:post_pk>/', include(p_router.urls)),
+    path('feed/', FeedView.as_view(), name="feed")
     
 ]
